c616d9aa4dad109c0bfcd9856d0a29c93a0f496d
wkeilsohn <kingwilliam42@gmail.com>
2020-01-20 21:03:32 -0500

    Added basic form

--- calculator/forms.py
+++ calculator/forms.py
@@ -1,2 +1,8 @@
 from django import forms
 
+class UploadForm(forms.Form):
+	file = forms.FileField(label = '.csv or .xlsx')
+
+class ContactForm(forms.Form):
+	email = forms.EmailField(label = 'Your Email:', required = True)
+	message = forms.CharField(label = 'Your Message:', max_length = 3000)
--- calculator/views.py
+++ calculator/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import *
 
 def index(request):
-	return render(request, 'home.html')
+	if request.method == 'POST':
+		form = UploadForm(request.POST)
+		if form.is_valid():
+			pass # Add and action here later.
+	else:
+		form = UploadForm()
+	return render(request, 'home.html', {'form':form})
 
 def about(request):
 	return render(request, 'about.html')

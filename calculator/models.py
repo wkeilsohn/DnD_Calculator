40776df317e2e83a76c051df67be6c4efe1f5b25
wkeilsohn <kingwilliam42@gmail.com>
2020-01-25 10:04:23 -0500

    Load data in DB

--- calculator/models.py
+++ calculator/models.py
@@ -2,21 +2,20 @@ from django.db import models
 
 class ChiTab(models.Model):
 	DegreeFreedom = models.IntegerField()
-	p_995 = models.IntegerField()
-	p_99 = models.IntegerField()
-	p_975 = models.IntegerField()
-	p_975 = models.IntegerField()
-	p_95 = models.IntegerField()
-	p_9 = models.IntegerField()
-	p_1 = models.IntegerField()
-	p_05 = models.IntegerField()
-	p_025 = models.IntegerField()
-	p_01 = models.IntegerField()
-	p_005 = models.IntegerField()
+	p_995 = models.FloatField()
+	p_99 = models.FloatField()
+	p_975 = models.FloatField()
+	p_95 = models.FloatField()
+	p_9 = models.FloatField()
+	p_1 = models.FloatField()
+	p_05 = models.FloatField()
+	p_025 = models.FloatField()
+	p_01 = models.FloatField()
+	p_005 = models.FloatField()
 
 	def to_dic(self):
 		return {'DegreeFreedom':self.DegreeFreedom, 'p_995':self.p_995,	'p_99':self.p_99, 'p_975':self.p_975,\
-		'p_975':self.p_975, 'p_95':self.p_95, 'p_9':self.p_9, 'p_1':self.p_1, 'p_05':self.p_05, 'p_025':self.p_025,\
+		'p_95':self.p_95, 'p_9':self.p_9, 'p_1':self.p_1, 'p_05':self.p_05, 'p_025':self.p_025,\
 		'p_01':self.p_01, 'p_005':self.p_005}
 
 
--- load.py
+++ load.py
@@ -1,9 +1,35 @@
 import pandas as pd
 from calculator.models import *
 
-#path = #Find file path.
-file = path + 'chitab.csv'
+path = "/home/william/Documents/Python_Scripts/DnD_Project/virtual/"
+file = path + 'chi_data.csv'
 
-df = pd.read_csv(file)
+def readFile():
+	df = pd.read_csv(file)
+	return df
 
+def addData():
+	df = readFile()
+	x = 0
+	for index, row in df.iterrows():
+		ls = []
+		for i in range(1, len(row)):
+			z = [float(row[i])]
+			ls = ls + z
+		try:
+			ch = ChiTab(DegreeFreedom=int(row[0]), p_995=ls[0], p_99=ls[1], p_975=ls[2],\
+				p_95=ls[3], p_9=ls[4], p_1=ls[5], p_05=ls[6], p_025=ls[7],\
+				p_01=ls[8], p_005=ls[9])
+			ch.save()
+		except:
+			x = x +1
+	print(str(x) + ' Rows failed to be added to the database.')
+
+def fullLoader():
+	try:
+		addData()
+	except:
+		print("Data Failed to Load")
+
+			
 
